0e2f6c5b6e3624d03ddfbe98ce42baeddee21286
tazo <tazo90@gmail.com>
2015-10-22 11:03:37 +0200

    more tests for remoteOptions directive

--- luxjs/tests/form-utils/remote.js
+++ luxjs/tests/form-utils/remote.js
@@ -1,4 +1,4 @@
- // Utility for creating a JSON form for testing
+    // Utility for creating a JSON form for testing
     var testFormUtils = {
         createForm: function () {
             var form = {
@@ -40,9 +40,9 @@
         //
         lux.formSelectUITests = {};
 
-        var controller;
         var api;
-        var $scope;
+        var scope;
+        var $timeout;
         var $compile;
         var $rootScope;
         var $httpBackend;
@@ -56,13 +56,6 @@
                 }
             }
         };
-
-        var apiMock;
-        var qMock;
-        var dataProvider;
-        var target = { url: 'dummy://url'};
-        var subPath = 'dummy/subPath';
-        var options = { dummy: 'options' };
 
         beforeEach(function () {
             module('lux.form.utils.test.selectui');
@@ -78,23 +71,30 @@
             api = $lux.api();
             $lux.resetAllSpies();
 
-            inject(function (_$compile_, _$rootScope_, _$httpBackend_) {
+            inject(function (_$timeout_, _$compile_, _$rootScope_, _$httpBackend_) {
+                $timeout = _$timeout_;
                 $compile = _$compile_;
                 $rootScope = _$rootScope_;
                 $httpBackend = _$httpBackend_;
 
                 $rootScope.formModelName = 'UserForm';
                 $rootScope.UserForm = {
-                    users_url: [{}]
+                    users: [{
+                      id: 1,
+                      name: 'test_option'
+                    }]
                 };
-            });
-        });
 
-        it("call directive", function() {
+                scope = $rootScope.$new();
+            });
+
             lux.formSelectUITests.select = testFormUtils.createForm({
                 type: 'select',
                 name: 'choice',
             });
+        });
+
+        it("call directive", function() {
             var element = testFormUtils.digest($compile, $rootScope,
                 "<div><lux-form data-options='lux.formSelectUITests.select'" +
                                 "data-remote-options='{\"url\": \"dummy://url\", \"name\": \"users_url\"}'>" +
@@ -104,36 +104,94 @@
         });
 
         it("check params", function() {
-            lux.formSelectUITests.select = testFormUtils.createForm({
-                type: 'select',
-                name: 'choice',
-            });
-
-
             var element = testFormUtils.digest($compile, $rootScope,
                 "<div><lux-form data-options='lux.formSelectUITests.select'" +
                                 "data-remote-options='{\"url\": \"dummy://url\", \"name\": \"users_url\"}'" +
                                 "data-remote-options-id='username'>" +
                         "</lux-form></div>");
 
-            //expect(api.get).toHaveBeenCalledWith(null, {limit:25, offset:0, username:null});
+            expect(api.get).toHaveBeenCalledWith(null, {limit:25, offset:0, username:null});
+        });
 
+        it("check remoteSearch()", function() {
+            var element = testFormUtils.digest($compile, $rootScope,
+                "<div><lux-form data-options='lux.formSelectUITests.select'" +
+                                "data-remote-options='{\"url\": \"dummy://url\", \"name\": \"users_url\"}'" +
+                                "data-remote-options-id='username'>" +
+                        "</lux-form></div>");
+
+            var select = {
+              search: 'test_user'
+            };
+
+            sc = element.scope();
+            // One to many field
+            sc.remoteSearch(select, false);
+            expect(api.get).toHaveBeenCalledWith(null, {limit:25, offset:0, username:'test_user'});
+            // Many to many field
+            sc.remoteSearch(select, true);
+            expect(api.get).toHaveBeenCalledWith(null, {limit:25, offset:0, name:'test_user'});
+            // Check empty
+            select = {search: ''};
+            sc.remoteSearch(select, false);
+            expect(api.get).toHaveBeenCalledWith(null, {limit:25, offset:0, name:null});
+        });
+
+        it("check multipleSelect()", function() {
+            var element = testFormUtils.digest($compile, $rootScope,
+                "<div><lux-form data-options='lux.formSelectUITests.select'" +
+                                "data-remote-options='{\"url\": \"dummy://url\", \"name\": \"users_url\"}'" +
+                                "data-remote-options-id='username' data-multiple='' data-name='users'>" +
+                        "</lux-form></div>");
+
+            var select = {
+              selected: []
+            };
+
+            expect(scope.UserForm.users.length).toBe(1);
+            sc = element.scope();
+            sc.multipleSelect(select, '');
+            expect(scope.UserForm.users.length).toBe(0);
+        });
+
+        it("check loadMore()", function() {
+            var element = testFormUtils.digest($compile, $rootScope,
+                "<div><lux-form data-options='lux.formSelectUITests.select'" +
+                                "data-remote-options='{\"url\": \"dummy://url\", \"name\": \"users_url\"}'" +
+                                "data-remote-options-id='username' data-name='users'>" +
+                        "</lux-form></div>");
+
+
+            sc = element.scope();
+            sc.loadMore();
+            $timeout.flush();
+            expect(api.get).toHaveBeenCalled();
+        });
+
+        it("populate options with response data on successful completion of get", function() {
+            var element = testFormUtils.digest($compile, $rootScope,
+                "<div><lux-form data-options='lux.formSelectUITests.select'" +
+                                "data-remote-options='{\"url\": \"dummy://url\", \"name\": \"users_url\"}'" +
+                                "data-remote-options-id='username' data-name='users'>" +
+                        "</lux-form></div>");
+
+            sc = element.scope();
             var thenSpy = $lux.getLastThenSpy();
             expect(thenSpy).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Function));
+            expect(sc.users_url.length).toBe(1);
+
             var successCallback = thenSpy.calls.mostRecent().args[0];
             successCallback({
                 data: {
                     result: [
                         {
-                            name: 'My Dataset',
-                            symbol: 'mse'
+                            id: 2,
+                            name: 'user_test'
                         }
                     ]
                 }
             });
 
-            //expect(controller.list).toEqual({ 'mse': 'My Dataset' });
-            expect(api.get).toHaveBeenCalledWith(null, {limit:25, offset:0, username:null});
-
+            expect(sc.users_url.length).toBe(2);
         });
     });
